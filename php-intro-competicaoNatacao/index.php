a7fd6887dbf80b40e0000fba223128925bae2ed3
isisfa <isis.falves@gmail.com>
2021-09-08 16:03:35 +0100

    receber dados via formuario de inscricao competicao de natacao

---
+++ php-intro-competicaoNatacao/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Inscrição</title>
+</head>
+<body>
+    <h1>Formulário para Inscrição de Competidores</h1>
+
+    <form action="script.php" method="post">
+        <p>Nome: <input type="text" name="nome" /> </p>
+        <p>Idade: <input type="number" name="idade" /> </p>
+        <p><input type="submit" value="Enviar" /></p>
+
+    </form>
+</body>
+</html>
--- php-intro-competicaoNatacao/script.php
+++ php-intro-competicaoNatacao/script.php
@@ -1,24 +1,8 @@
 <?php
 
-/* Competição natação
-*Inserção de competidores: nome, idade
-*Categoria: 6 a 12 anos infantil, 13 a 18 adolescente, acima de 18 adulto
-*Retornar a categoria para cadas nadador que for cadastrado */
-
-// $categorias = [];
-// $categorias[] = 'infantil';
-// $categorias[] = 'adolescente';
-// $categorias[] = 'adulto';
-
 $categorias = ['infantil', 'adolescente', 'adulto'];
-$nome = 'Luiz';
-$idade = 4;
-
-// print_r($categorias);
-// echo'</br>';
-// var_dump($nome);
-// echo '</br>';
-// var_dump($idade);
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
 
 if ($idade >= 6 && $idade <= 12) {
     for ($i = 0; $i < count($categorias); $i++) 
