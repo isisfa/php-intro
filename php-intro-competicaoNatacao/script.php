d71805cb39b778ec2902eed0d39976b9da0b1a96
isisfa <isis.falves@gmail.com>
2021-09-09 15:57:08 +0100

    criacao de servicos e funcoes php

--- php-intro-competicaoNatacao/script.php
+++ php-intro-competicaoNatacao/script.php
@@ -1,74 +1,12 @@
 <?php
-    session_start();
 
-$categorias = ['infantil', 'adolescente', 'adulto'];
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-if(empty($nome))
-{
-    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, por favor preencha-o novamente';
-    header('location: index.php');
-    return;
-}
+defineCategoriaCompetidor($nome, $idade);
 
-    else if(strlen($nome) < 3) 
-{
-    $_SESSION['mensagem-de-erro'] = 'O nome deve conter mais de 3 caracteres';
-    header('location: index.php');
-    return;
-}
-
-    else if(strlen($nome) > 40)
-{
-    $_SESSION['mensagem-de-erro'] = 'O nome é muito extenso';
-    header('location: index.php');
-    return;
-}
-
-    else if (!is_numeric($idade) || $idade > 100 || empty($idade))
-{
-    $_SESSION['mensagem-de-erro'] = 'Informe a idade';
-    header('location: index.php');
-    return;
-}
-
-
- if ($idade >= 6 && $idade <= 12) 
-{
-    for ($i = 0; $i < count($categorias); $i++) 
-    {
-        if ($categorias[$i] === 'infantil')
-        {
-            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' .$nome. ' compete na categoria infantil';
-            header('location: index.php');
-            return;
-        }
-    }
-}
-    else if ($idade >=13 && $idade <= 18) 
-{
-    for ($i = 0; $i < count($categorias); $i++) 
-    {
-        if ($categorias[$i] === 'adolescente') 
-        {
-            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' .$nome. ' compete na categoria adolescente';
-            header('location: index.php');
-            return;
-        }
-    }
-} 
-else 
-{
-    for ($i = 0; $i < count($categorias); $i++) 
-    {
-        if ($categorias[$i] === 'adulto')
-        {
-            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' .$nome. ' compete na categoria adulto';
-            header('location: index.php');
-            return;
-        }
-    }
- }
-
+header('location: index.php');
